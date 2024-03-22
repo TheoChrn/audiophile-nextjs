df2c1fc70a5aba738c2b430a76df869d2bd16ca2
Theo Cheron <theo.chrn@gmail.com>
2024-03-22 14:45:11 +0100

    Update route.ts

--- src/app/api/categories/[slug]/route.ts
+++ src/app/api/categories/[slug]/route.ts
@@ -4,7 +4,7 @@ import data from "../../../../data/data.json"
 
 export async function GET(request: NextRequest, context: any) {
   const {params: {slug}} = context
-  const products = data.filter((product) => product.category === slug).sort((a, b) => (+b.new) - (+a.new));
+  const products = data.filter((product) => product.category === slug)
   
   if (products === undefined || !products) {
     return NextResponse.json(null)
